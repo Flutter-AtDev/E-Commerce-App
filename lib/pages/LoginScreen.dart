bb15ffcf07feaa04bf4d28c7958fa00dcd6bba3e
AhmedTawfiqM <ahmedadelmm26@yahoo.com>
2020-05-30 20:45:51 +0200

    ""

--- lib/pages/LoginScreen.dart
+++ lib/pages/LoginScreen.dart
@@ -5,6 +5,7 @@ import 'package:ecommerceapp/utils/Colors.dart';
 import 'package:ecommerceapp/utils/Constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -14,86 +15,105 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final GlobalKey<FormState> _globalKey = GlobalKey<FormState>();
   String email, password;
+  bool isLoading=false;
 
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: MAINCOLOR,
-      body: Form(
-        key: _globalKey,
-        child: ListView(
-          children: <Widget>[
-            SizedBox(
-              height: height * .16,
-            ),
-            CustomLogoApp(),
-            SizedBox(height: height * .08),
-            CustomTextFiled(
-                onClick: (value) {
-                  email = value;
-                },
-                hint: EMAIL_HINT,
-                icon: Icons.email),
-            CustomTextFiled(
-                onClick: (value) {
-                  password = value;
-                },
-                hint: Password_HINT,
-                icon: Icons.lock),
-            SizedBox(
-              height: height * .02,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 120),
-              child: FlatButton(
-                onPressed: () async{
-                  bool isValidate = _globalKey.currentState.validate();
-                  if (isValidate) {
-                    _globalKey.currentState.save();
-                    print('$email : $password');
-                   final result =  await AuthApp().Login(email, password);
-                   print(result.user.uid);
-                    //LoginUser();
-                  }
-                },
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                color: Colors.black,
-                child: Text(
-                  'Login',
-                  style: TextStyle(
-                    color: Colors.white,
+      body: ModalProgressHUD(
+        inAsyncCall: isLoading,
+        child: Form(
+          key: _globalKey,
+          child: ListView(
+            children: <Widget>[
+              SizedBox(
+                height: height * .16,
+              ),
+              CustomLogoApp(),
+              SizedBox(height: height * .08),
+              CustomTextFiled(
+                  onClick: (value) {
+                    email = value;
+                  },
+                  hint: EMAIL_HINT,
+                  icon: Icons.email),
+              CustomTextFiled(
+                  onClick: (value) {
+                    password = value;
+                  },
+                  hint: Password_HINT,
+                  icon: Icons.lock),
+              SizedBox(
+                height: height * .02,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 120),
+                child: Builder(
+                  builder:(context)=> FlatButton(
+                    onPressed: () async{
+                      bool isValidate = _globalKey.currentState.validate();
+                      if (isValidate) {
+                        setState(() {
+                          isLoading=true;
+                        });
+                        _globalKey.currentState.save();
+                        try {
+                          print('$email : $password');
+                          final result = await AuthApp().Login(email, password);
+                          print(result.user.uid);
+                        }catch(e){
+                          setState(() {
+                            isLoading=false;
+                          });
+                          print(e);
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text(e.message),
+                          ));
+                        }
+                        //LoginUser();
+                      }
+                    },
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    color: Colors.black,
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: height * .07,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Dont Have Account ? ',
-                  style: TextStyle(color: Colors.white, fontSize: 17),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pushNamed(context, SIGNUPSCREEN_ID);
-                  },
-                  child: Text(
-                    'Sign Up',
-                    style: TextStyle(color: Colors.black, fontSize: 18),
+              SizedBox(
+                height: height * .07,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Dont Have Account ? ',
+                    style: TextStyle(color: Colors.white, fontSize: 17),
                   ),
-                )
-              ],
-            ),
-            SizedBox(
-              height: height * .06,
-            )
-          ],
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, SIGNUPSCREEN_ID);
+                    },
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(color: Colors.black, fontSize: 18),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: height * .06,
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/pages/SignUpScreen.dart
+++ lib/pages/SignUpScreen.dart
@@ -5,6 +5,7 @@ import 'package:ecommerceapp/utils/Colors.dart';
 import 'package:ecommerceapp/utils/Constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class SignUpScreen extends StatefulWidget {
   @override
@@ -14,7 +15,7 @@ class SignUpScreen extends StatefulWidget {
 class _SignUpScreenState extends State<SignUpScreen> {
   GlobalKey<FormState> globalKey = GlobalKey<FormState>();
   String name, email, password;
-
+  bool isLoading=false;
   //
   @override
   void initState() {
@@ -29,87 +30,106 @@ class _SignUpScreenState extends State<SignUpScreen> {
     //
     return Scaffold(
       backgroundColor: MAINCOLOR,
-      body: Form(
-        key: globalKey,
-        child: ListView(
-          children: <Widget>[
-            SizedBox(
-              height: height * .16,
-            ),
-            CustomLogoApp(),
-            SizedBox(height: height * .08),
-            CustomTextFiled(
-                onClick: (value) {
-                  name = value;
-                },
-                hint: NAME_HINT,
-                icon: Icons.person),
-            CustomTextFiled(
-                onClick: (value) {
-                  email = value;
-                },
-                hint: EMAIL_HINT,
-                icon: Icons.email),
-            CustomTextFiled(
-                onClick: (value) {
-                  password = value;
-                },
-                hint: Password_HINT,
-                icon: Icons.lock),
-            SizedBox(
-              height: height * .02,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 120),
-              child: FlatButton(
-                onPressed: () async {
-                  print('pressed...');
-                  bool isValidate = globalKey.currentState
-                      .validate(); // Force Rebuild to call VAlidators in TextFormField
-                  if (isValidate) {
-                    globalKey.currentState.save();
-                    print('$email : $password');
-                    final result =  await AuthApp().SignUp(email, password);
-                    print(result.user.uid);
-                  }
-                },
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                color: Colors.black,
-                child: Text(
-                  'Sign up',
-                  style: TextStyle(
-                    color: Colors.white,
+      body: ModalProgressHUD(
+        inAsyncCall: isLoading,
+        child: Form(
+          key: globalKey,
+          child: ListView(
+            children: <Widget>[
+              SizedBox(
+                height: height * .16,
+              ),
+              CustomLogoApp(),
+              SizedBox(height: height * .08),
+              CustomTextFiled(
+                  onClick: (value) {
+                    name = value;
+                  },
+                  hint: NAME_HINT,
+                  icon: Icons.person),
+              CustomTextFiled(
+                  onClick: (value) {
+                    email = value;
+                  },
+                  hint: EMAIL_HINT,
+                  icon: Icons.email),
+              CustomTextFiled(
+                  onClick: (value) {
+                    password = value;
+                  },
+                  hint: Password_HINT,
+                  icon: Icons.lock),
+              SizedBox(
+                height: height * .02,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 120),
+                child: Builder(
+                  builder: (context) => FlatButton(
+                    onPressed: () async {
+                      print('pressed...');
+                      bool isValidate = globalKey.currentState
+                          .validate(); // Force Rebuild to call VAlidators in TextFormField
+                      if (isValidate) {
+                        setState(() {
+                          isLoading=true;
+                        });
+                        globalKey.currentState.save();
+                        print('$email : $password');
+                        try {
+                          final result = await AuthApp().SignUp(email, password);
+                          print(result.user.uid);
+                          Navigator.pushNamed(context, HOMESCREEN_ID);
+                        } catch (e) {
+                          setState(() {
+                            isLoading=false;
+                          });
+                          print(e);
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text(e.message),
+                          ));
+                        }
+                      }
+                    },
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    color: Colors.black,
+                    child: Text(
+                      'Sign up',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: height * .07,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Have an Account ? ',
-                  style: TextStyle(color: Colors.white, fontSize: 17),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pushNamed(context, LOGINSCREEN_ID);
-                  },
-                  child: Text(
-                    'Login',
-                    style: TextStyle(color: Colors.black, fontSize: 18),
+              SizedBox(
+                height: height * .07,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Have an Account ? ',
+                    style: TextStyle(color: Colors.white, fontSize: 17),
                   ),
-                )
-              ],
-            ),
-            SizedBox(
-              height: height * .06,
-            )
-          ],
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, LOGINSCREEN_ID);
+                    },
+                    child: Text(
+                      'Login',
+                      style: TextStyle(color: Colors.black, fontSize: 18),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: height * .06,
+              )
+            ],
+          ),
         ),
       ),
     );
